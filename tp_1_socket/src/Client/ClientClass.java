fcac594972f29e93c7ca9544a54f89763fc25753
alfonso.ramirez <is713932@iteso.mx>
2021-09-29 00:41:12 +0200

    Feat: Initial commit for PART 1
    Added the initial files for the server and client, along with basic java structure made in VsCode

---
+++ tp_1_socket/src/Client/ClientClass.java
@@ -0,0 +1,29 @@
+package Client;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class ClientClass {
+    public static void main(String argv[]) {
+        try {
+            Socket socketClient = new Socket("localhost", 5555);
+            System.out.println("Client: " + "Connection Established");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+            String serverMsg;
+            writer.write("8\r\n");
+            writer.write("10\r\n");
+            writer.flush();
+            while ((serverMsg = reader.readLine()) != null) {
+                System.out.println("Client:" + serverMsg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
